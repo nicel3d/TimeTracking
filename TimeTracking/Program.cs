b60ca450033acb5d189a84b6644256ff1c7b624f
nikel <nicel_05@mail.ru>
2019-03-30 19:15:59 +0700

    Add project files.

---
+++ TimeTracking/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimeTracking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
