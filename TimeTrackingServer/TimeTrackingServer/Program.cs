c42f29fa38074215f8d55118b6c9d1d7b80ee739
nikel <nicel_05@mail.ru>
2019-05-07 21:56:38 +0700

    серврер, откл. браузер, Use Kestrel
    - отключить запуск браузера для сервера,
    - использовать UseKestrel для кроссбраузерности сервера

--- TimeTrackingServer/TimeTrackingServer/Program.cs
+++ TimeTrackingServer/TimeTrackingServer/Program.cs
@@ -1,6 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 namespace TimeTrackingServer
 {
@@ -16,6 +17,16 @@ namespace TimeTrackingServer
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+            .UseKestrel(options =>
+            {
+                options.Limits.MaxConcurrentConnections = 100;
+                options.Limits.MaxRequestBodySize = 10 * 1024;
+                options.Limits.MinRequestBodyDataRate =
+                    new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
+                options.Limits.MinResponseDataRate =
+                    new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
+                //options.Listen(IPAddress.Loopback, 5000);
+            });
     }
 }
